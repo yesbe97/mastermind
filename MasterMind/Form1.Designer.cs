8f5efd1ab32e20603b0eb85a9496b45b9ec110f4
yesbe97 <vandrielnicky@gmail.com>
2016-09-21 10:06:27 +0200

    Base commit

---
+++ MasterMind/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace MasterMind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guessBox1 = new System.Windows.Forms.TextBox();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // guessBox1
+            // 
+            this.guessBox1.Location = new System.Drawing.Point(13, 13);
+            this.guessBox1.Name = "guessBox1";
+            this.guessBox1.Size = new System.Drawing.Size(100, 20);
+            this.guessBox1.TabIndex = 0;
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(13, 40);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(75, 23);
+            this.checkButton.TabIndex = 1;
+            this.checkButton.Text = "Controleren";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.guessBox1);
+            this.Name = "Form1";
+            this.Text = "MasterMind";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox guessBox1;
+        private System.Windows.Forms.Button checkButton;
+    }
+}
+
---
+++ MasterMind/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MasterMind
+{
+    public partial class Form1 : Form
+    {
+        private string solution1;
+        public Form1()
+        {
+            InitializeComponent();
+
+            Random random = new Random();
+            solution1 = random.Next(1, 6).ToString();
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            if (guessBox1.Text == solution1)
+            {
+                guessBox1.BackColor = Color.LightGreen;
+            }
+            else
+            {
+                guessBox1.BackColor = Color.LightGray;
+            }
+        }
+    }
+}
