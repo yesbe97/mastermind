19688f19ec13c933c7bb18d681e724340e870af4
yesbe97 <vandrielnicky@gmail.com>
2016-10-02 23:31:45 +0200

    cimmit

--- MasterMind/Form1.Designer.cs
+++ MasterMind/Form1.Designer.cs
@@ -30,18 +30,21 @@
         {
             this.guessBox1 = new System.Windows.Forms.TextBox();
             this.checkButton = new System.Windows.Forms.Button();
+            this.guessBox2 = new System.Windows.Forms.TextBox();
+            this.guessBox3 = new System.Windows.Forms.TextBox();
+            this.guessBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // guessBox1
             // 
-            this.guessBox1.Location = new System.Drawing.Point(13, 13);
+            this.guessBox1.Location = new System.Drawing.Point(13, 12);
             this.guessBox1.Name = "guessBox1";
-            this.guessBox1.Size = new System.Drawing.Size(100, 20);
+            this.guessBox1.Size = new System.Drawing.Size(20, 20);
             this.guessBox1.TabIndex = 0;
             // 
             // checkButton
             // 
-            this.checkButton.Location = new System.Drawing.Point(13, 40);
+            this.checkButton.Location = new System.Drawing.Point(26, 38);
             this.checkButton.Name = "checkButton";
             this.checkButton.Size = new System.Drawing.Size(75, 23);
             this.checkButton.TabIndex = 1;
@@ -49,11 +52,35 @@
             this.checkButton.UseVisualStyleBackColor = true;
             this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
             // 
+            // guessBox2
+            // 
+            this.guessBox2.Location = new System.Drawing.Point(39, 12);
+            this.guessBox2.Name = "guessBox2";
+            this.guessBox2.Size = new System.Drawing.Size(20, 20);
+            this.guessBox2.TabIndex = 2;
+            // 
+            // guessBox3
+            // 
+            this.guessBox3.Location = new System.Drawing.Point(66, 12);
+            this.guessBox3.Name = "guessBox3";
+            this.guessBox3.Size = new System.Drawing.Size(20, 20);
+            this.guessBox3.TabIndex = 3;
+            // 
+            // guessBox4
+            // 
+            this.guessBox4.Location = new System.Drawing.Point(92, 12);
+            this.guessBox4.Name = "guessBox4";
+            this.guessBox4.Size = new System.Drawing.Size(20, 20);
+            this.guessBox4.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.guessBox4);
+            this.Controls.Add(this.guessBox3);
+            this.Controls.Add(this.guessBox2);
             this.Controls.Add(this.checkButton);
             this.Controls.Add(this.guessBox1);
             this.Name = "Form1";
@@ -67,6 +94,9 @@
 
         private System.Windows.Forms.TextBox guessBox1;
         private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.TextBox guessBox2;
+        private System.Windows.Forms.TextBox guessBox3;
+        private System.Windows.Forms.TextBox guessBox4;
     }
 }
 
--- MasterMind/Form1.cs
+++ MasterMind/Form1.cs
@@ -13,23 +13,100 @@ namespace MasterMind
     public partial class Form1 : Form
     {
         private string solution1;
+        private string solution2;
+        private string solution3;
+        private string solution4;
+        private List<int> random_list;
+        private List<int> checkBoxCondition_list;
+
         public Form1()
         {
             InitializeComponent();
 
             Random random = new Random();
-            solution1 = random.Next(1, 6).ToString();
-        }
+            int solution1 = random.Next(1, 6);
+            int solution2 = random.Next(1, 6);
+            int solution3 = random.Next(1, 6);
+            int solution4 = random.Next(1, 6);
 
+            random_list = new List<int>();
+            random_list.Add(solution1);
+            random_list.Add(solution2);
+            random_list.Add(solution3);
+            random_list.Add(solution4);
+        }
+   
         private void checkButton_Click(object sender, EventArgs e)
         {
-            if (guessBox1.Text == solution1)
+            checkBoxCondition_list = new List<int>();
+            if (guessBox1.Text == random_list[0].ToString())
             {
                 guessBox1.BackColor = Color.LightGreen;
+                checkBoxCondition_list.Add(2);
+            }
+            else if (random_list.Contains(int.Parse(guessBox1.Text)))
+            {
+                guessBox1.BackColor = Color.Yellow;
+                checkBoxCondition_list.Add(1);
             }
             else
             {
                 guessBox1.BackColor = Color.LightGray;
+                checkBoxCondition_list.Add(0);
+            }
+            ///////////////////////////////////////////////////////////
+            if (guessBox2.Text == random_list[1].ToString())
+            {
+                guessBox2.BackColor = Color.LightGreen;
+                checkBoxCondition_list.Add(2);
+            }
+            else if (random_list.Contains(int.Parse(guessBox2.Text)))
+            {
+                guessBox2.BackColor = Color.Yellow;
+                checkBoxCondition_list.Add(1);
+            }
+            else
+            {
+                guessBox2.BackColor = Color.LightGray;
+                checkBoxCondition_list.Add(0);
+            }
+            ///////////////////////////////////////////////////////////
+            if (guessBox3.Text == random_list[2].ToString())
+            {
+                guessBox3.BackColor = Color.LightGreen;
+                checkBoxCondition_list.Add(2);
+            }
+            else if (random_list.Contains(int.Parse(guessBox3.Text)))
+            {
+                guessBox3.BackColor = Color.Yellow;
+                checkBoxCondition_list.Add(1);
+            }
+            else
+            {
+                guessBox3.BackColor = Color.LightGray;
+                checkBoxCondition_list.Add(0);
+            }
+            ///////////////////////////////////////////////////////////
+            if (guessBox4.Text == random_list[3].ToString())
+            {
+                guessBox4.BackColor = Color.LightGreen;
+                checkBoxCondition_list.Add(2);
+            }
+            else if (random_list.Contains(int.Parse(guessBox4.Text)))
+            {
+                guessBox4.BackColor = Color.Yellow;
+                checkBoxCondition_list.Add(1);
+            }
+            else
+            {
+                guessBox4.BackColor = Color.LightGray;
+                checkBoxCondition_list.Add(0);
+            }
+            ///////////////////////////////////////////////////////////
+            if (!checkBoxCondition_list.Contains(0) || !checkBoxCondition_list.Contains(1))
+            {
+                MessageBox.Show(checkBoxCondition_list.ToString());
+                //MessageBox.Show("Je hebt de juiste code gevonden");
             }
         }
     }
